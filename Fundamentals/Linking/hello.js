a10982a897841819cd3279d8c6e4f1aad7d4e792
azammustafa66 <azammustafa66@gmail.com>
2023-04-29 12:11:12 +0530

    Day 1

---
+++ Fundamentals/Linking/hello.js
@@ -0,0 +1,19 @@
+const country = 'India'
+const continent = 'Asia'
+let population = 1400000000
+const isIsland = false
+let language = "Urdu"
+
+++population
+
+console.log(country, continent, population, isIsland, language)
+
+// country = "China"
+// continent = "Europe"
+// isIsland = true
+
+// console.log(country, continent, population, isIsland, language)
+
+arr = [10, 58, 30, 85]
+
+console.log(arr)
